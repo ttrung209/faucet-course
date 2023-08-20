d72b88cde1fcd2649663b08386f42d3f80ac5eb7
LE TUAN TRUNG <39075355+ttrung209@users.noreply.github.com>
2023-08-20 11:53:48 +0700

    Add Learning_Solidity_from_scratch

---
+++ contracts/Learning_Solidity_from_scratch/set_get.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0;
+
+contract firstcontract{
+    uint saveData;
+
+    function set(uint x) public {
+        saveData = x;
+    }
+
+    function get() public view returns (uint x) {
+        return saveData;
+    }
+}
